8613ba0157749758cd6da42513a271cbefb94d5f
Dane Balia <daneb@tuta.io>
2021-12-25 22:40:22 +0200

    Tutorial for ini

---
+++ csstranspiler.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/alecthomas/repr"
+	"os"
+
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
+)
+
+var iniLexer = lexer.MustSimple([]lexer.Rule{
+	{`Ident`, `[a-zA-Z][a-zA-Z_\d]*`, nil},
+	{`String`, `"(?:\\.|[^"])*"`, nil},
+	{`Float`, `\d+(?:\.\d+)?`, nil},
+	{`Punct`, `[][=]`, nil},
+	{"comment", `[#;][^\n]*`, nil},
+	{"whitespace", `\s+`, nil},
+})
+
+type INI struct {
+	Properties []*Property `@@*`
+	Sections   []*Section  `@@*`
+}
+
+type Property struct {
+	Key   string `@Ident "="`
+	Value *Value `@@`
+}
+
+type Value struct {
+	Pos    lexer.Position
+	String *string  ` @String`
+	Number *float64 `| @Float`
+}
+
+type Section struct {
+	Identifier string      `"[" @Ident "]"`
+	Properties []*Property `@@*`
+}
+
+var parser = participle.MustBuild(&INI{},
+	participle.Lexer(iniLexer),
+	participle.Unquote("String"),
+)
+
+func main() {
+	ini := &INI{}
+	err := parser.Parse("", os.Stdin, ini)
+	if err != nil {
+		panic(err)
+	}
+	repr.Println(ini, repr.Indent("  "), repr.OmitEmpty(true))
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module csstranspiler
+
+go 1.17
+
+require (
+	github.com/alecthomas/participle/v2 v2.0.0-alpha7
+	github.com/alecthomas/repr v0.0.0-20181024024818-d37bc2a10ba1
+	github.com/stretchr/testify v1.4.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
